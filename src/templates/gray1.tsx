044f0a1ce3a02868ead87fd3484726479b9ed318
Duval Kilpatrick <mkilpatrick@yext.com>
2023-11-07 14:56:43 -0500

    fix

--- src/templates/gray1.tsx
+++ src/templates/gray1.tsx
@@ -3,6 +3,8 @@ import { templateForColor } from "../ColorBase";
 import {Color} from "../PageLayout"
 
 const template = templateForColor(Color.GRAY)
-export const getPath = template.getPath + "/1";
+export const getPath = () => {
+    return Color.GRAY.toString() + "/1";
+};
 export const getHeadConfig = template.getHeadConfig;
 export default template.default;
